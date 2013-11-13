60979ef13cb693bc7c866f2e3b51d48a14ad7c35
patmood <patrick.n.moody@gmail.com>
2013-11-13 16:48:01 +1030

    refactor gif load function

--- js/control.js
+++ js/control.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
 
-  $('#huge').click(function(){ loadGif(++gifPos); });
-  $('#next').click(function(){ loadGif(++gifPos); });
-  $('#prev').click(function(){ loadGif(--gifPos); });
+  $('#huge').click(function(){ loadGif(gifList[++gifPos]); });
+  $('#next').click(function(){ loadGif(gifList[++gifPos]); });
+  $('#prev').click(function(){ loadGif(gifList[--gifPos]); });
 
   var gifList = [];
   var gifPos = 0;
@@ -21,14 +21,14 @@ $(document).ready(function(){
               });
             }
         });
-        if (gifPos === 0) loadGif(gifPos);
+        if (gifPos === 0) loadGif(gifList[gifPos]);
         console.log(gifList);
     });
   }
 
   function loadGif(gifData){
-    $('#huge').html($('<img/>').attr("src", gifList[gifData].url));
-    $('#title').html(gifList[gifData].title)
+    $('#huge').html($('<img/>').attr("src", gifData.url));
+    $('#title').html(gifData.title)
     console.log(gifPos);
     if (gifPos >= gifList.length-15) getMoreGifs();
   }
@@ -42,9 +42,9 @@ $(document).ready(function(){
     var key = e.which
 
     // arrow keys
-    if(key === 32) loadGif(++gifPos) // space
-    else if (key === 39) loadGif(++gifPos) // right
-    else if (key === 37) loadGif(--gifPos) // left
+    if(key === 32) loadGif(gifList[++gifPos]) // space
+    else if (key === 39) loadGif(gifList[++gifPos]) // right
+    else if (key === 37) loadGif(gifList[--gifPos]) // left
     // else if (key === 38) vote("up") // up
     // else if (key === 40) vote("down") // down
   })
